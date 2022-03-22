67de99c7b768d7a229c757675e746a91e19aa8ef
Yunus <yecicen@gmail.com>
2022-03-22 20:44:45 +0000

    yunus changed

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,8 +1,13 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
+
+fetch('http://localhost:3000/api/data').then(response => response.json())
+.then(data => console.log(data));
+
 function draw() {
     const svgWidth = 500;
     const svgHeight = 500;
+    getInformation()
 
     let svg = d3.select("svg");
     let width = svg
